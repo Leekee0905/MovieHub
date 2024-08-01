44f51ac8d6e8de6bae849994e4f3e156971be9d1
JIWI-CHOUX <als.dn960418@gmail.com>
2024-08-01 14:59:00 +0900

    fix: index 파일수정

--- detail/js/index.js
+++ detail/js/index.js
@@ -1,6 +1,8 @@
 import renderCarousel from "./carousel.js";
 import createHeaderSearchInput from "../../js/header.js";
 import makeCommentsBox from "./comment.js";
+import review from "./review-input.js";
 createHeaderSearchInput();
 renderCarousel();
 makeCommentsBox();
+review();
