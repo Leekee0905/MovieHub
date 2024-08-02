a5eee3a4002a05c5b2877fb2b0282ae4c7ed6fe9
stopbrother <stopbro0323@naver.com>
2024-08-02 09:59:10 +0900

    pr하기전 commit

--- detail/js/index.js
+++ detail/js/index.js
@@ -1,6 +1,5 @@
 import renderCarousel from "./carousel.js";
 import createHeaderSearchInput from "../../js/header.js";
-import makeCommentsBox from "./comment.js";
+
 createHeaderSearchInput();
 renderCarousel();
-makeCommentsBox();
