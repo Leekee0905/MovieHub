fa06124658070effacce21fd2f1775b07f557e61
GiSeong Lee <cj8928@gmail.com>
2024-08-06 16:47:48 +0900

    fix: 언어변경 새로고침에서 리렌더링으로 변경

--- js/language.js
+++ js/language.js
@@ -1,3 +1,7 @@
+import { drawCards } from "./card.js";
+import { getUpcomeList } from "./getData.js";
+import { randomMovie } from "./upcoming.js";
+
 export const language = () => {
   const root = document.getElementById("root");
   const upcomingContainer = document.getElementById("upcoming-container");
@@ -23,10 +27,23 @@ export const language = () => {
   koBtn.addEventListener("click", () => changeLanguage("ko"));
   enBtn.addEventListener("click", () => changeLanguage("en"));
 
-  const changeLanguage = (language) => {
+  const changeLanguage = async (language) => {
     localStorage.setItem("language", language);
-    location.reload();
+    const active = Number(document.querySelector(".active").innerHTML);
+    const upcomeContainer = document.querySelector("#upcoming-container");
+    const upComeData = await getUpcomeList();
+    const upComingMovies = upComeData.results;
+    const upCome = upComingMovies[Math.floor(Math.random() * upComingMovies.length)];
+    const comeCard = randomMovie(upCome);
+    upcomeContainer.innerHTML = comeCard;
+    await drawCards(active);
   };
 
   root.insertBefore(languageBtnContainer, upcomingContainer.nextSibling);
+};
+
+export const getLanguage = () => {
+  const language = localStorage.getItem("language") || "en-US";
+  console.log(`Current language: ${language}`);
+  return language;
 };
